3c32d5b5e52fc66e774971318989361a76907d2d
Benjamin Wong <bycw@uw.edu>
2025-02-11 17:08:48 -0800

    rename

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def g(x1,x2):
 x_max = 1
 dx = 250 
 x = np.linspace(0,x_max, dx)
-u_max = [0.01, np.pi]
+u_max = [0.1, np.pi]
 X,Y = np.meshgrid(x,x)
 
 mus = [np.array([0.5,0.5])]
@@ -71,40 +71,40 @@ ax.set_aspect('equal', adjustable='box')
 plt.contourf(X,Y, pi, extent=[0,x_max, 0,x_max], cmap='Greys', levels = 100)
 
 #%% single agent 
-x = np.random.rand(2)
-s = [x.copy()]
-pis = []
-for i in range(50000):
-    pi = get_pdf(X,Y,mus,sigmas, [0.5, 0.5])
-    pis.append(pi.copy())
-    x_prime = g_sample(x)
-    g1 = g(x_prime,x)
-    g2 = g(x,x_prime)
-    A = np.min([1, (f(x_prime, pi)*g1)/((f(x, pi)*g2))])
-    if np.random.rand() <= A:
-        x=x_prime
-    s.append(x.copy())
+# x = np.random.rand(2)
+# s = [x.copy()]
+# pis = []
+# for i in range(50000):
+#     pi = get_pdf(X,Y,mus,sigmas, [0.5, 0.5])
+#     pis.append(pi.copy())
+#     x_prime = g_sample(x)
+#     g1 = g(x_prime,x)
+#     g2 = g(x,x_prime)
+#     A = np.min([1, (f(x_prime, pi)*g1)/((f(x, pi)*g2))])
+#     if np.random.rand() <= A:
+#         x=x_prime
+#     s.append(x.copy())
    
-s = np.array(s)    
+# s = np.array(s)    
 #%%    
-fig = plt.figure()    
-ax = plt.gca()
-ax.set_aspect('equal', adjustable='box')
-plt.axis([0, x_max, 0, x_max])
-plt.axis('square')
-plt.ylim(0, x_max)
-plt.xlim(0 , x_max)
-def animate(i):
-    ax.clear()
-    ax.set_xlim(0, x_max)
-    ax.set_ylim(0, x_max)
-    dist = ax.contourf(X,Y, pis[i], extent=[0,x_max, 0,x_max], cmap='Greys', levels = 100)
-    swarm = ax.plot(s[i*10,0], s[i*10,1], ".", color="red",  markersize = 10)
-    past =  ax.plot(s[:i*10,0], s[:i*10,1], ".", color="red",  markersize = 5, alpha = 0.1)
-    return past+swarm+[dist]
+# fig = plt.figure()    
+# ax = plt.gca()
+# ax.set_aspect('equal', adjustable='box')
+# plt.axis([0, x_max, 0, x_max])
+# plt.axis('square')
+# plt.ylim(0, x_max)
+# plt.xlim(0 , x_max)
+# def animate(i):
+#     ax.clear()
+#     ax.set_xlim(0, x_max)
+#     ax.set_ylim(0, x_max)
+#     dist = ax.contourf(X,Y, pis[i], extent=[0,x_max, 0,x_max], cmap='Greys', levels = 100)
+#     swarm = ax.plot(s[i*10,0], s[i*10,1], ".", color="red",  markersize = 10)
+#     past =  ax.plot(s[:i*10,0], s[:i*10,1], ".", color="red",  markersize = 5, alpha = 0.1)
+#     return past+swarm+[dist]
         
-ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=int(len(s)/20))    
-ani.save("metropolis_hasting_ergodic.gif", dpi=300, writer=PillowWriter(fps=25))
+# ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=int(len(s)/20))    
+# ani.save("metropolis_hasting_ergodic.gif", dpi=300, writer=PillowWriter(fps=25))
 #%%
 mus = [np.array([0.8,0.4]), np.array([0.2,0.8])]
 sigmas = [np.array([[0.01, 0.005], [0.005, 0.01]]), np.array([[0.01, 0.0], [0, 0.01]])]
@@ -122,8 +122,8 @@ x = np.random.rand(NUM_AGENT, 2)
 s = [x.copy()]
 pis = []
 for i in range(1000):
-  #  mus = [np.array([0.8,0.4]), np.array([0.2+0.001*i,0.8])]
-    mus =  [np.array([0.5,0.5])]
+    mus = [np.array([0.8,0.4]), np.array([0.2+0.001*i,0.8])]
+    # mus =  [np.array([0.5,0.5])]
     phi = get_pdf(X,Y,mus,sigmas, [0.5, 0.5])
     pis.append(phi.copy())
     x = metropolis_hasting_par(x, [dx,dx], phi, u_max)
@@ -165,23 +165,23 @@ for i in range(int(len(s)/10)):
     plt.pause(0.01)
     plt.close()
 #%%
-step = 2
-fig = plt.figure()    
-ax = plt.gca()
-ax.set_aspect('equal', adjustable='box')
-plt.axis([0, x_max, 0, x_max])
-plt.axis('square')
-plt.ylim(0, x_max)
-plt.xlim(0 , x_max)
-def animate(i):
-    ax.clear()
-    ax.set_xlim(0, x_max)
-    ax.set_ylim(0, x_max)
-    dist = ax.contourf(X,Y, pis[i*step], extent=[0,x_max, 0,x_max], cmap='Greys', levels = 100)
-    swarm = ax.plot(s[i*step,:,0], s[i*step,:,1], ".", color="red",  markersize = 10)
+# step = 2
+# fig = plt.figure()    
+# ax = plt.gca()
+# ax.set_aspect('equal', adjustable='box')
+# plt.axis([0, x_max, 0, x_max])
+# plt.axis('square')
+# plt.ylim(0, x_max)
+# plt.xlim(0 , x_max)
+# def animate(i):
+#     ax.clear()
+#     ax.set_xlim(0, x_max)
+#     ax.set_ylim(0, x_max)
+#     dist = ax.contourf(X,Y, pis[i*step], extent=[0,x_max, 0,x_max], cmap='Greys', levels = 100)
+#     swarm = ax.plot(s[i*step,:,0], s[i*step,:,1], ".", color="red",  markersize = 10)
    
-    return swarm+[dist]
+#     return swarm+[dist]
         
-ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=len(s)//step)    
-# ani.save("metropolis_hasting_swarm.gif", dpi=300, writer=PillowWriter(fps=25))
-ani.save("chemotaxis.gif", dpi=300, writer=PillowWriter(fps=25))
+# ani = FuncAnimation(fig, animate, interval=40, blit=True, repeat=True, frames=len(s)//step)    
+# # ani.save("metropolis_hasting_swarm.gif", dpi=300, writer=PillowWriter(fps=25))
+# ani.save("chemotaxis.gif", dpi=300, writer=PillowWriter(fps=25))
